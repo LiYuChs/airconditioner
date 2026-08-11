640a750f7b5bdcf740105aedeae21bc9bc47f7cc
LiChS <lee.alex.tw@gmail.com>
2026-08-11 15:14:25 +0800

    fix 新增佈署作業

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 完成 Google Apps Script 部署後，把網頁應用程式網址貼在引號中。
-window.COOL_HOURS_API_URL = 'https://script.google.com/macros/s/AKfycbze3xtG9K1aKZ_uRTtqLksYt5Enf7IXNoLWoQNQ73MNFmx7uyG9Rt8AOLTtZmVxQ876vA/exec';
+window.COOL_HOURS_API_URL = 'https://script.google.com/macros/s/AKfycbxxVY70U_4DKqAMrLFC00LSf2CEFU3n-zuDxsFZ1rnZwsxlHNDgzKByltjLovXFgj2E9w/exec';
